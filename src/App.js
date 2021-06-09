ba5d91407726c587af7bb8e0aa8662e369c449a1
kiraci <>
2021-06-09 13:22:52 +0300

    Basic notions and setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar"
+import { Greet } from './components/Greet'
+import Counter  from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter />
+        <Greet name="ahmet" />
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Greet.js
@@ -0,0 +1,7 @@
+import { React } from 'react'
+
+export const Greet = (probs) => {
+    console.log(probs);
+    return <h1>Hello, {probs.name}</h1>
+}
+
